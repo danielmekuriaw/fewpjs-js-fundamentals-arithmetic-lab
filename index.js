f8ad4c13aa47be6afea8ec3d3f34128f43c62062
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-08-12 22:54:50 -0500

    commit tree 87edf992b23c2d31bb9f0530793631f2fee9863d

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+let random = getRandomInt(100)
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+let num3 = 9
+let num4 = 5
+let mod = num3 % num4
+
+let max = Math.max(1, 3, 2, 20)
